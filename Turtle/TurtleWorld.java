fe95a72ee703e2b122a0ae59fe88355ad0bb161f
jtisaac <jtisaac@NN123-HA-21.HS.sd203.org>
2015-08-21 15:08:49 -0500

    Continued to explore the TurtleWorld interface and methods. AP Com Sci A day 3

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -4,19 +4,17 @@ public class TurtleWorld
 {
     public static void main(String args[])
     {
-        World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
-        turtle.penDown();
-        turtle.forward(50);
-        turtle.penUp();
-        turtle.clearPath();
-        turtle.turnRight();
-        turtle.forward(1000);
-        turtle.turnRight();
-        turtle.turnRight();
-        turtle.forward(1000);
-       
+       World turtleWorld = new World();
+       Turtle turtle = new Turtle(turtleWorld);
+       turtle.turn(45);
+       int x = 1;
+       while (x < 1000)
+       {turtle.turn(45);
+           turtle.forward(x);
+           x+= 1;
+           
         
     }
+       Turtle turtle1 = new Turtle(turtleWorld);
+}
 }
