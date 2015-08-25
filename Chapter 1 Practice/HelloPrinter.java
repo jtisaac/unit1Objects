8ac605e202b66739944a0dd0779a374c3770eb3d
jtisaac <jtisaac@NN123-HA-21.HS.sd203.org>
2015-08-25 15:01:43 -0500

    Using Java and images 8/25/15

---
+++ Chapter 1 Practice/HelloPrinter.java
@@ -0,0 +1,9 @@
+public class HelloPrinter
+{
+    public static void main( String[] args)
+    {
+        // display a gteeting in the console window
+        
+        System.out.println( "Hello, World!" );
+    }
+}thsi si se
---
+++ Chapter 1 Practice/testr.java
@@ -0,0 +1,9 @@
+
+public class testr
+{
+    public static void main(String[] args)
+    {
+        System.out.println("An emergency Broadcast");
+    }
+}
+    
